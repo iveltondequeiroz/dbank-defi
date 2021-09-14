b792d8b6fcfddb7e1efa83b6f34b9675f3c52e29
iveltondequeiroz <iveltondequeirozgmail.com>
2021-09-14 12:30:00 -0400

    migrate token contract

--- src/contracts/Token.sol
+++ src/contracts/Token.sol
@@ -6,13 +6,19 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 contract Token is ERC20 {
   address public minter;
 
-  //add minter changed event
+  event MinterChanged(address indexed from, address to);
 
   constructor() public payable ERC20("Stars", "STR") {
     minter = msg.sender;
   }
 
-  //Add pass minter role function
+  function passMinterRole(address dBank) public returns (bool) {
+    require(msg.sender==minter, 'Error, Only owner can change minter role');
+		minter = dBank;
+
+    emit MinterChanged(msg.sender, dBank);
+    return true;
+  }
 
   function mint(address account, uint256 amount) public {
     //check if msg.sender have minter role
